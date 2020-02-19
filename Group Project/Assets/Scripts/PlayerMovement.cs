7b7bfee26da52aadefce07ecccc42ba1b70400f4
Luke Reckard <luke.reckard@gmail.com>
2020-02-19 14:32:59 -0800

    Characters + dialogue

--- Group Project/Assets/Scripts/PlayerMovement.cs
+++ Group Project/Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -12,26 +13,31 @@ public class PlayerMovement : MonoBehaviour
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
 
-    void Start ()
+    public Text characterName;
+    public GameObject dialogueBackground;
+    public Text dialogue;
+
+
+    void Start()
     {
-        m_Animator = GetComponent<Animator> ();
-        m_Rigidbody = GetComponent<Rigidbody> ();
-        m_AudioSource = GetComponent<AudioSource> ();
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
-        float horizontal = Input.GetAxis ("Horizontal");
-        float vertical = Input.GetAxis ("Vertical");
-        
-        m_Movement.Set(horizontal, 0f, vertical);
-        m_Movement.Normalize ();
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
-        m_Animator.SetBool ("IsWalking", isWalking);
-        
+        m_Animator.SetBool("IsWalking", isWalking);
+
         if (isWalking)
         {
             if (!m_AudioSource.isPlaying)
@@ -41,16 +47,44 @@ public class PlayerMovement : MonoBehaviour
         }
         else
         {
-            m_AudioSource.Stop ();
+            m_AudioSource.Stop();
         }
 
-        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation (desiredForward);
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
     }
 
-    void OnAnimatorMove ()
+    void OnAnimatorMove()
     {
-        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
-        m_Rigidbody.MoveRotation (m_Rotation);
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
+    //Move into dialogue script aka dialogue.text = Dialogue.GetDialogue("Punchy")
+    void OnCollisionStay(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "Punchy")
+        {
+            characterName.gameObject.SetActive(true);
+            dialogueBackground.SetActive(true);
+            dialogue.gameObject.SetActive(true);
+            characterName.text = "Punchy";
+            dialogue.text = "Just chilling in the bathroom. I wonder where Rosie is... maybe her room?";
+        }
+        else if (collision.gameObject.name == "Rosie")
+        {
+            characterName.gameObject.SetActive(true);
+            dialogueBackground.SetActive(true);
+            dialogue.gameObject.SetActive(true);
+            characterName.text = "Punchy";
+            dialogue.text = "Detective you have to get me out of here! I cannot believe I am couped up in this ratty hotel.";
+        }
+        else
+        {
+            characterName.gameObject.SetActive(false);
+            dialogueBackground.SetActive(false);
+            dialogue.gameObject.SetActive(false);
+        }
     }
 }
