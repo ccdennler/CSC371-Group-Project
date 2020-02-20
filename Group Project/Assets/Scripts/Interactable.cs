f02738ff6f1c7bcab7b26ec88693af3342e48f3b
Georgia Monahan <38015950+grgia@users.noreply.github.com>
2020-02-20 10:20:23 -0800

    Initial item scripts

---
+++ Group Project/Assets/Scripts/Interactable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+ public class Interactable : MonoBehaviour{
+
+    public NavMeshAgent playerAgent;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("woot wooot");
+        }
+    }
+
+    public virtual void MoveToInteraction(NavMeshAgent playerAgent)
+    {
+        this.playerAgent = playerAgent;
+        playerAgent.destination = this.transform.position;
+        Interact();
+    }
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with base class.");
+    }
+}
